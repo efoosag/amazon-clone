b83b94b5c9cb097a40d858c023bfd0328ae560e0
Victor E. Osagie <efoosagsite@gmail.com>
2023-08-07 19:52:26 +0100

    add home page carousel

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import HomePageCarousel from './HomePageCarousel';
 
 const HomePage = () => (
-  <div>HomePage</div>
+  <div>
+    <HomePageCarousel />
+    ;
+    <div>HomePage</div>
+    ;
+  </div>
+
 );
 
 export default HomePage;
